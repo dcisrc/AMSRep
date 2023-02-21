d37d1a5f05810e8eccb63fc8177114eb6f3ef92d
DBP Data Center, Inc <rpaganan@dci.ph>
2023-02-21 14:44:31 +0800

    push

--- AMSRep/db_connect.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$conn= new mysqli('localhost','root','','assets')or die("Could not connect to mysql".mysqli_error($con));
-
-
-
-?>
--- AMSRep/pdo_connect.php
+++
@@ -1,7 +0,0 @@
-<?php  
-
-$db = new PDO("mysql:host=localhos;dbname=assets;charset=utf8mb4;'root',''");
-
-
-
-?>
